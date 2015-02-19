d75c44d2c46b63bbe0d470f141250bbbf4db5e93
unknown <nbalboni2@gmail.com>
2015-02-19 15:28:09 -0500

    Some minor stuff

--- stocks.py
+++ stocks.py
@@ -19,14 +19,14 @@ signal.signal(signal.SIGINT, signal_handler)
 
 class Stock:
 	data     = {}
+	name     = ""
+	bid      = ""
+	ask      = ""
 	url      = 'https://query.yahooapis.com/v1/public/yql'
 	sql_stmt = 'SELECT Name, Bid, Ask, LastTradePriceOnly FROM yahoo.finance.quotes WHERE symbol="{}"'
 
 	def __init__(self, ticker):
 		self.ticker              = ticker
-		self.name                = ""
-		self.bid                 = ""
-		self.ask                 = ""
 		self.data['q']           = re.sub(r'\{\}', ticker, self.sql_stmt)
 		self.data['format']      = 'json'
 		self.data['diagnostics'] = 'false'
@@ -38,30 +38,34 @@ class Stock:
 	def refresh_data(self):
 		try:
 			response  = json.load(urllib2.urlopen(self.full_url))
-			self.name = response['query']['results']['quote']['Name']
+			if not (self.name):
+				self.name = response['query']['results']['quote']['Name']
 			self.bid  = response['query']['results']['quote']['Bid']
 			self.ask  = response['query']['results']['quote']['Ask']
 		except urllib2.HTTPError, e:
 			print "HTTPError = " + str(e.code)
-			sys.exit(0)
 		except urllib2.URLError, e:
 			print "URLError = " + str(e.reason)
-			sys.exit(0)
 		except httplib.HTTPException, e:
 			print "HTTPException"
-			sys.exit(0)
 		except Exception:
 			print "Unknown exception"
-			sys.exit(0)
 
 	def get_name_string(self):
-		return self.name + ' (' + self.ticker + ')'
+		if (self.name):
+			return self.name + ' (' + self.ticker + ')'
+		return self.ticker
 
 	def get_bid_string(self):
-		return "Bid : " + self.bid
+		if (self.bid):
+			return " Bid : " + self.bid
+		return " Bid : N/A"
+
 
 	def get_ask_string(self):
-		return "Ask : " + self.ask
+		if (self.ask):
+			return " Ask : " + self.ask
+		return " Ask : N/A"
 
 
 stocks = []
