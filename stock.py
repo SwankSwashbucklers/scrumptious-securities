88743b611e7a97cfd27919dff1d2d4a1c66662c1
unknown <nbalboni2@gmail.com>
2015-02-26 12:52:15 -0500

    Now with scraping for values and formated floats and multi inheritance and all that good stuff

--- stock.py
+++ stock.py
@@ -1,15 +1,62 @@
+# -*- coding: utf-8 -*-
 import urllib, urllib2, httplib
 import json, re
 
-class Stock(object):
-	stock_data = {}
-	endpoint   = ""
+class Scraper_Stock(object):
+	scraped_data = {}
+	endpoint     = ""
+
+	def __init__(self, ticker):
+		self.setup(ticker)
+
+	def setup(self, ticker):
+		req_data = {}
+		req_data['s'] = ticker.lower()
+		self.endpoint = 'http://finance.yahoo.com/q?' + urllib.urlencode(req_data)
+
+		self.scraped_data['Name']  = ""
+		self.scraped_data['Value'] = 0.0
+		self.refresh_value()
+
+	def make_request(self):
+		try:
+			return urllib2.urlopen(self.endpoint).read()
+		except urllib2.HTTPError, e:
+			print "HTTPError = " + str(e.code)
+		except urllib2.URLError, e:
+			print "URLError = " + str(e.reason)
+		except httplib.HTTPException, e:
+			print "HTTPException"
+		except Exception:
+			print "Unknown exception"
+		return None
+
+	def refresh_value(self): #TODO: add checking if regex cant find match
+		response = self.make_request()
+		if response is None:
+			return
+
+		self.scraped_data['Value'] = float(
+			str(re.search(r'[0-9\.]*$', 
+			str(re.search(r'<span[\w\s="]*class="time_rtq_ticker[\w\s="]*><span[\w\s="]*>[\w\s\.]*', response).group())).group()))
+
+		if not (self.scraped_data['Name']):
+			self.scraped_data['Name'] = str(re.search(r'(?<=: Summary for ).*(?=-)', 
+				str(re.search(r'(?<=<title>).*(?=<\/title>)', response).group())).group())
+
+
+
+class YQL_Stock(object):
+	stock_data     = {}
+	yql_endpoint   = ""
 
 	def __init__(self, fields, ticker):
-		self.stock_data['Ticker']  = ticker
+		self.yql_setup(fields, ticker)
+
+	def yql_setup(self, fields, ticker):
 		field_string = ""
 		for f in range(0, len(fields)):
-			self.stock_data[fields[f]] = ""
+			self.stock_data[fields[f]] = None
 			field_string += fields[f]
 			if (f < (len(fields)-1)):
 				field_string += ", "
@@ -22,12 +69,12 @@ class Stock(object):
 		req_data['diagnostics']  = 'false'
 		req_data['env']          = 'store://datatables.org/alltableswithkeys'
 		req_data['callback']     = ''
-		self.endpoint = 'https://query.yahooapis.com/v1/public/yql?' + urllib.urlencode(req_data)
+		self.yql_endpoint = 'https://query.yahooapis.com/v1/public/yql?' + urllib.urlencode(req_data)
 		self.refresh_data()
 
-	def make_request(self):
+	def make_yql_request(self):
 		try:
-			return json.load(urllib2.urlopen(self.endpoint))
+			return json.load(urllib2.urlopen(self.yql_endpoint))
 		except urllib2.HTTPError, e:
 			print "HTTPError = " + str(e.code)
 		except urllib2.URLError, e:
@@ -39,51 +86,64 @@ class Stock(object):
 		return None
 
 	def refresh_data(self):
-		response = self.make_request()
+		response = self.make_yql_request()
 		if response is None: 
 			quote = None 
 		else:
 			quote = response['query']['results']['quote']
 		
 		for key in self.stock_data:
-			if key == 'Ticker': continue
-			if quote is None: 
-				self.stock_data[key] = None 
-			else:
+			if key == 'Ticker': 
+				continue
+			if not quote is None: 
 				self.stock_data[key] = quote[key]
 
 
-class TickerStock(Stock):
-	name   = ""
-	ticker = ""
-	bid    = ""
-	ask    = ""
+
+class TickerStock(Scraper_Stock, YQL_Stock):
+	name           = ""
+	ticker         = ""
+	value          = 0.0
+	previous_close = 0.0
+	open_price     = 0.0
 
 	def __init__(self, ticker):
-		fields = ['Name', 'Bid', 'Ask']
-		super(TickerStock, self).__init__(fields, ticker)
+		self.ticker = ticker
+		fields = ['PreviousClose', 'Open']
+		super(TickerStock, self).setup(ticker)
+		super(TickerStock, self).yql_setup(fields, ticker)
+
+	def refresh_value(self):
+		super(TickerStock, self).refresh_value()
+		if not (self.name):
+			self.name = self.scraped_data['Name']
+		self.value = self.scraped_data['Value']
 
 	def refresh_data(self):
 		super(TickerStock, self).refresh_data()
-		if not (self.name):
-			self.name = self.stock_data['Name']
-		if not (self.ticker):
-			self.ticker = self.stock_data['Ticker']
-		self.bid = self.stock_data['Bid']
-		self.ask = self.stock_data['Ask']
+		self.previous_close = float(self.stock_data['PreviousClose'])
+		self.open_price     = float(self.stock_data['Open'])
 
 	def get_name_string(self):
 		if (self.name):
 			return self.name + ' (' + self.ticker + ')'
 		return self.ticker
 
-	def get_bid_string(self):
-		if not self.bid is None:
-			return " Bid : " + self.bid
-		return " Bid : N/A"
-
-	def get_ask_string(self):
-		if not self.ask is None:
-			return " Ask : " + self.ask
-		return " Ask : N/A"
+	def get_value_string(self):
+		if (self.value):
+			if (self.previous_close):
+				if (self.value > self.previous_close):
+					dir_string = "\x1E"
+				elif (self.value < self.previous_close):
+					dir_string = "\x1F"
+				else:
+					dir_string = "\xFE"
+				percent = ((self.value - self.previous_close)/self.previous_close)*100
+				percent_string = '%.3f' % percent
+			else:
+				dir_string = "\xF9"
+				percent_string = "--.---"
+			value_string = '%.2f' % self.value
+			return " " + value_string + " " + dir_string + " " + percent_string + " %"
+		return " " + "N/A"
 
--- tickers.py
+++ tickers.py
@@ -4,17 +4,16 @@ import math, time, os
 import sys, signal
 
 MAX_LENGTH   = 80
-H_BUFFER     = 2
-V_BUFFER     = 4
-REFRESH_RATE = 5
-WATCHLIST    = ['TSLA', 'GOOGL', 'GOOG', 'AAPL']#, 'OMCL', 'AAL', 'LUV', 'GPRO', 'ERII', 
-				#'AMZN', 'NFLX', 'MA', 'GM', 'F', 'HMC', 'HEMP', 'PHOT']
+H_BUFFER     = 5
+V_BUFFER     = 3
+REFRESH_RATE = 10
+WATCHLIST    = ['TSLA', 'GOOGL', 'GOOG', 'AAPL', 'OMCL', 'ERII']
+#WATCHLIST    = ['TSLA', 'GOOGL', 'GOOG', 'AAPL', 'OMCL', 'AAL', 'LUV', 'GPRO', 'ERII', 
+#				'AMZN', 'NFLX', 'MA', 'GM', 'F', 'HMC', 'HEMP', 'PHOT']
 
 
 def signal_handler(signal, frame):
-        print "\nScript terminated by user \xCE"
-        print "\x1E"
-        print "\x1F"
+        print "\nScript terminated by user"
         sys.exit(0)
 
 os.system('cls' if os.name == 'nt' else 'clear')
@@ -31,13 +30,11 @@ while (1):
 	longest = 0
 
 	for stock in stocks:
-		stock.refresh_data()
+		stock.refresh_value()
 		if (longest < len(stock.get_name_string())): 
 			longest = len(stock.get_name_string())
-		if (longest < len(stock.get_bid_string())): 
-			longest = len(stock.get_bid_string())
-		if (longest < len(stock.get_ask_string())): 
-			longest = len(stock.get_ask_string())
+		if (longest < len(stock.get_value_string())): 
+			longest = len(stock.get_value_string())
 
 	columns = int(math.floor(MAX_LENGTH/float(longest+H_BUFFER)))
 	#if ((columns*longest+(columns-1)*H_BUFFER) < MAX_LENGTH): 
@@ -45,26 +42,23 @@ while (1):
 	rows    = int(math.ceil(len(stocks)/float(columns)))
 
 	for r in range(0, rows):
-		for x in range(0, 3):
+		for x in range(0, 2):
 			output.append("")
 		for c in range(0, columns):
 			if (c+(r*columns) >= len(stocks)):
 				break
 			stock = stocks[c+(r*columns)]
-			output[(0)+(r*3)] += stock.get_name_string()
-			output[(1)+(r*3)] += stock.get_bid_string()
-			output[(2)+(r*3)] += stock.get_ask_string()
+			output[(0)+(r*2)] += stock.get_name_string()
+			output[(1)+(r*2)] += stock.get_value_string()
 			if not (c==(columns-1)):
 				for x in range(0, (longest-len(stock.get_name_string()))+H_BUFFER):
-					output[(0)+(r*3)] += " "
-				for x in range(0, (longest-len(stock.get_bid_string()))+H_BUFFER):
-					output[(1)+(r*3)] += " "
-				for x in range(0, (longest-len(stock.get_ask_string()))+H_BUFFER):
-					output[(2)+(r*3)] += " "
+					output[(0)+(r*2)] += " "
+				for x in range(0, (longest-len(stock.get_value_string()))+H_BUFFER):
+					output[(1)+(r*2)] += " "
 
 	os.system('cls' if os.name == 'nt' else 'clear')
 	for l in range(0, len(output)):
-		if (l % 3 == 0) and not (l == 0):
+		if (l % 2 == 0) and not (l == 0):
 			for x in range(0, V_BUFFER):
 				print ""
 		print output[l]
