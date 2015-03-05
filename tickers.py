b041caaea0041c7fc24e8f32995385c4a23a12ff
unknown <nbalboni2@gmail.com>
2015-03-05 16:23:56 -0500

    Tab with spaces is cleaner

--- tickers.py
+++ tickers.py
@@ -8,7 +8,7 @@ V_BUFFER     = 3
 REFRESH_RATE = 10
 WATCHLIST    = ['TSLA', 'AAPL', 'GOOGL', 'GOOG', 'OMCL', 'ERII', 'SCTY']
 #WATCHLIST    = ['TSLA', 'GOOGL', 'GOOG', 'AAPL', 'OMCL', 'AAL', 'LUV', 'GPRO', 'ERII', 
-#				'AMZN', 'NFLX', 'MA', 'GM', 'F', 'HMC', 'HEMP', 'PHOT']
+#               'AMZN', 'NFLX', 'MA', 'GM', 'F', 'HMC', 'HEMP', 'PHOT']
 
 
 def signal_handler(signal, frame):
@@ -21,44 +21,44 @@ signal.signal(signal.SIGINT, signal_handler)
 
 stocks = []
 for ticker in WATCHLIST:
-	stocks.append(TickerStock(ticker))
+    stocks.append(TickerStock(ticker))
 
 
 while (1):
-	output  = []
-	longest = 0
+    output  = []
+    longest = 0
 
-	for stock in stocks:
-		stock.scraper.refresh_data()
-		if (longest < len(stock.get_name_string())): 
-			longest = len(stock.get_name_string())
-		if (longest < len(stock.get_value_string())): 
-			longest = len(stock.get_value_string())
+    for stock in stocks:
+        stock.scraper.refresh_data()
+        if (longest < len(stock.get_name_string())): 
+            longest = len(stock.get_name_string())
+        if (longest < len(stock.get_value_string())): 
+            longest = len(stock.get_value_string())
 
-	columns = int(math.floor(MAX_LENGTH/float(longest+H_BUFFER)))
-	#if ((columns*longest+(columns-1)*H_BUFFER) < MAX_LENGTH): 
-	#	columns = columns + 1
-	rows    = int(math.ceil(len(stocks)/float(columns)))
+    columns = int(math.floor(MAX_LENGTH/float(longest+H_BUFFER)))
+    #if ((columns*longest+(columns-1)*H_BUFFER) < MAX_LENGTH): 
+    #   columns = columns + 1
+    rows    = int(math.ceil(len(stocks)/float(columns)))
 
-	for r in range(0, rows):
-		for x in range(0, 2):
-			output.append("")
-		for c in range(0, columns):
-			if (c+(r*columns) >= len(stocks)):
-				break
-			stock = stocks[c+(r*columns)]
-			output[(0)+(r*2)] += stock.get_name_string()
-			output[(1)+(r*2)] += stock.get_value_string()
-			if not (c==(columns-1)):
-				for x in range(0, (longest-len(stock.get_name_string()))+H_BUFFER):
-					output[(0)+(r*2)] += " "
-				for x in range(0, (longest-len(stock.get_value_string()))+H_BUFFER):
-					output[(1)+(r*2)] += " "
+    for r in range(0, rows):
+        for x in range(0, 2):
+            output.append("")
+        for c in range(0, columns):
+            if (c+(r*columns) >= len(stocks)):
+                break
+            stock = stocks[c+(r*columns)]
+            output[(0)+(r*2)] += stock.get_name_string()
+            output[(1)+(r*2)] += stock.get_value_string()
+            if not (c==(columns-1)):
+                for x in range(0, (longest-len(stock.get_name_string()))+H_BUFFER):
+                    output[(0)+(r*2)] += " "
+                for x in range(0, (longest-len(stock.get_value_string()))+H_BUFFER):
+                    output[(1)+(r*2)] += " "
 
-	os.system('cls' if os.name == 'nt' else 'clear')
-	for l in range(0, len(output)):
-		if (l % 2 == 0) and not (l == 0):
-			for x in range(0, V_BUFFER):
-				print()
-		print(output[l])
-	time.sleep(REFRESH_RATE)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    for l in range(0, len(output)):
+        if (l % 2 == 0) and not (l == 0):
+            for x in range(0, V_BUFFER):
+                print()
+        print(output[l])
+    time.sleep(REFRESH_RATE)
